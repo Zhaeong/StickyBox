607656f1c6b102785acefbac114d3a37af69bd76
Zhaeong <owen_zzz@hotmail.com>
2017-12-31 15:59:33 -0500

    added binding feature for lines to player

--- scripts/moveAnim.gd
+++ scripts/moveAnim.gd
@@ -23,15 +23,20 @@ func initilizeLines():
 	for i in range(0, numLines):
 		var lineStart_x = rand_range(player_pos.x - (player_size.x/2), player_pos.x + (player_size.x/2))
 		var lineEnd_x = rand_range(player_pos.x - (player_size.x/2) - 40, player_pos.x + (player_size.x/2) + 40)
-		var lineStart = Vector2(lineStart_x, player_pos.y + (player_size.y / 2))
+		
+		var lineStart = Vector2(player_pos.x - lineStart_x, player_pos.y - player_pos.y + (player_size.y / 2))
+		
 		var lineEnd = Vector2(lineEnd_x, ground_pos.y)
 		
 		var lineObj = [lineStart, lineEnd]
 		lineArray.append(lineObj)
 		
 func drawLines():
+	var player_pos = get_node("../player").get_pos()
+	
 	for i in range(0, lineArray.size()):
-		draw_line(lineArray[i][0], lineArray[i][1], Color(0, 90, 90), 1)
+		var lineStart = Vector2(player_pos.x + lineArray[i][0][0], player_pos.y + lineArray[i][0][1])
+		draw_line(lineStart, lineArray[i][1], Color(0, 90, 90), 1)
 		
 func updateLines():
-	
+	pass
