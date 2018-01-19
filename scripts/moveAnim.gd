6ca8a80e287d18979143d0955d5c930f928b11c6
Zhaeong <owen_zzz@hotmail.com>
2018-01-18 23:45:12 -0500

    made the movement from side to side and random stick drawing
    what now...

--- scripts/moveAnim.gd
+++ scripts/moveAnim.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var lineArray = []
 
-var numLinesStart = 4
+var numLinesStart = 6
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
@@ -16,27 +16,48 @@ func _process(delta):
 	self.update()
 
 func initilizeLines():
-	var player_pos = get_node("../playerMain/player").get_pos()
-	var player_size = get_node("../playerMain/player").get_item_rect().size
-	var ground_pos = get_node("../ground").get_pos()
+	var player_sprite_pos = get_node("../playerMain/player").get_pos()
+	var player_sprite_size = get_node("../playerMain/player").get_item_rect().size
 	
-	for i in range(0, numLinesStart):
-		createLinePoint(player_pos, player_size, ground_pos)
+	var player_pos = get_node("../playerMain").get_pos()	
+	
+	var ground_pos = get_node("../ground").get_pos()
 
-func createLinePoint(playerPos, playerSize, groundPos):
-	var lineStart_x = rand_range(playerPos.x - (playerSize.x/2), playerPos.x + (playerSize.x/2))
-	var lineEnd_x = rand_range(playerPos.x - (playerSize.x/2) - 40, playerPos.x + (playerSize.x/2) + 40)
+	for i in range(0, numLinesStart):
+		createLinePoint(player_pos, player_sprite_size, ground_pos, 20, 20)
+
+func createLinePoint(playerPos, playerSize, groundPos, groundLeftOffset, groundRightOffset):
+	var lineStart_x = rand_range(playerPos.x - (playerSize.x/2) + 10, playerPos.x + (playerSize.x/2) -10)
+	var lineEnd_x = rand_range(playerPos.x - (playerSize.x/2) - groundLeftOffset, playerPos.x + (playerSize.x/2) + groundRightOffset)
 	
 	var lineStart = Vector2(playerPos.x - lineStart_x, playerPos.y - playerPos.y + (playerSize.y / 2))
 	var lineEnd = Vector2(lineEnd_x, groundPos.y)
 	lineArray.append([lineStart, lineEnd])
 	return [lineStart, lineEnd]
-		
+
+func deleteLowestLinePoint():
+	var least = lineArray[0][1].x
+	var index = 0
+	for i in range(0, numLinesStart):
+		var pointVal = lineArray[i][1].x
+		if pointVal < least:
+			index = i
+	lineArray.remove(index)
+	
+func deleteGreatestLinePoint():
+	var greatest = lineArray[0][1].x
+	var index = 0
+	for i in range(0, numLinesStart):
+		var pointVal = lineArray[i][1].x
+		if pointVal > greatest:
+			index = i
+	lineArray.remove(index)
+	
 func drawLines():
 	var player_pos = get_node("../playerMain").get_pos()
 	for i in range(0, lineArray.size()):
 		var lineStart = Vector2(player_pos.x + lineArray[i][0][0], player_pos.y + lineArray[i][0][1])
-		draw_line(lineStart, lineArray[i][1], Color(0, 90, 90), 1)
+		draw_line(lineStart, lineArray[i][1], Color(0, 0, 0), 3)
 		
 func updateLines():
 	pass
--- scripts/objectCollider.gd
+++ scripts/objectCollider.gd
@@ -3,19 +3,64 @@ extends Area2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+var rightNode
+var leftNode
 
+var colliderOffset = 70
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	get_node("../colid").connect("body_enter",self,"_on_Area2D_body_enter")
-	get_node("../colid").connect("body_exit",self,"_on_Area2D_body_exit")
-	pass
+	get_node("../colid_right").connect("body_enter",self,"_on_Area2D_body_enter_right")
+	get_node("../colid_right").connect("body_exit",self,"_on_Area2D_body_exit")
+	
+	get_node("../colid_left").connect("body_enter",self,"_on_Area2D_body_enter_left")
+	
+	rightNode = get_node("../colid_right")
+	leftNode = get_node("../colid_left")
+	set_process(true)
 
 
-func _on_Area2D_body_enter( body ):
-	var player_pos = get_node("../playerMain/player").get_pos()
-	var player_size = get_node("../playerMain/player").get_item_rect().size
+func _process(delta):
+	var player_pos = get_node("../playerMain").get_pos()
+	var new_pos_y = player_pos.y
+	var new_pos_right_x = rightNode.get_pos().x
+	var new_pos_left_x = leftNode.get_pos().x
+	if(abs(rightNode.get_pos().x - player_pos.x) > colliderOffset):
+		new_pos_right_x = player_pos.x + colliderOffset
+	
+	if(abs(player_pos.x - leftNode.get_pos().x) > colliderOffset):
+		new_pos_left_x = player_pos.x - colliderOffset
+	
+	var new_pos_right = Vector2(new_pos_right_x, player_pos.y)
+	var new_pos_left = Vector2(new_pos_left_x, player_pos.y)
+		
+	rightNode.set_pos(new_pos_right)
+	leftNode.set_pos(new_pos_left)
+	
+func _on_Area2D_body_enter_right( body ):
+	var player_sprite_pos = get_node("../playerMain/player").get_pos()
+	var player_sprite_size = get_node("../playerMain/player").get_item_rect().size
 	var ground_pos = get_node("../ground").get_pos()
 	
-	get_node("../moveAnim").createLinePoint(player_pos, player_size, ground_pos)
-	pass # replace with function body
+	var player_pos = get_node("../playerMain").get_pos()	
+	
+	get_node("../moveAnim").createLinePoint(player_pos, player_sprite_size, ground_pos, -20, 100)
+	get_node("../moveAnim").deleteLowestLinePoint()
+	#set new positon
+	
+	var new_pos = Vector2(player_pos.x + 100, player_pos.y)
+	rightNode.set_pos(new_pos)
+	
+func _on_Area2D_body_enter_left( body ):
+	var player_sprite_pos = get_node("../playerMain/player").get_pos()
+	var player_sprite_size = get_node("../playerMain/player").get_item_rect().size
+	var ground_pos = get_node("../ground").get_pos()
+	
+	var player_pos = get_node("../playerMain").get_pos()	
+	
+	get_node("../moveAnim").createLinePoint(player_pos, player_sprite_size, ground_pos, 100, -20)
+	get_node("../moveAnim").deleteGreatestLinePoint()
+	#set new positon
+	
+	var new_pos = Vector2(player_pos.x - 100, player_pos.y)
+	leftNode.set_pos(new_pos)
